a8c9679d4a44df5039988db281755be71b5753bf
Minto108 <158478797+Minto108@users.noreply.github.com>
2025-02-11 21:21:54 +0530

    Update first_char.java

--- first_char.java
+++ first_char.java
@@ -3,10 +3,7 @@ public class first_char{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the word: ");
-        char g = s.next().charAt(0);
-        // or
-        System.out.println("Enter the word: ");
-        char word = s.next();
+        String word = s.next();
         char f = word.charAt(0);
     }
 }
